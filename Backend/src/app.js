d7147933d35d4b8e06a17cd56ba7930b0724c4b2
PranavIIITN <pranavtiwari98@gmail.com>
2026-03-30 01:50:31 +0530

    Server setup

---
+++ Backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import authRoutes from "./modules/auth/auth.routes";  // importing authentication route
+
+const app = express();
+
+// 1. Core middleware
+app.use(express.json()); 
+
+// 2. Auth Route
+app.use("/auth", authRoutes); 
+
+// 3. Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message });
+});
+
+
+
+
+
+
+
+
+
---
+++ Backend/src/index.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`WithinBlocks running on port ${PORT}`);
+})
